59a362781119af16524675402cb6dd62e6880c7d
Andrew Mullan <Andrew.Mullan@sage.com>
2024-02-23 13:21:41 -0800

    update comments

--- src/Searchlight/Exceptions/InvalidOperation.cs
+++ src/Searchlight/Exceptions/InvalidOperation.cs
@@ -1,10 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+#pragma warning disable CS1591
 namespace Searchlight.Exceptions
 {
+    /// <summary>
+    /// The operation used in the filter on a given field is not supported.
+    ///
+    /// Example: `(someField gt 5)` where `someField` is an encrypted field.
+    /// </summary>
     public class InvalidOperation : SearchlightException
     {
         public string OriginalFilter { get; internal set; }
